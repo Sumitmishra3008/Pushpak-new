c1cae6347d43da5f8cceb99df7001afc4ae62c98
Sumitmishra3008 <submit.mishra2002@gmail.com>
2026-04-01 05:28:15 +0530

    figured out the google apis

--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -4,7 +4,6 @@ const captainModel = require('../models/captain.model');
 module.exports.getAddressCoordinate = async (address) => {
     const apiKey = process.env.GOOGLE_MAPS_API;
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
-
     try {
         const response = await axios.get(url);
         if (response.data.status === 'OK') {
@@ -30,7 +29,6 @@ module.exports.getDistanceTime = async (origin, destination) => {
     const apiKey = process.env.GOOGLE_MAPS_API;
 
     const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
-
     try {
 
 
